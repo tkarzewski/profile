b2e2b08a41e72fbff203ea9fff44562c07d37a65
Tobias Karzewski <tobias@karzewski.de>
2018-09-08 09:41:19 +0200

    Fixed Charset in Mail-Workaround

--- src/assets/php/contact-workaround.php
+++ src/assets/php/contact-workaround.php
@@ -12,19 +12,34 @@ if (!isset($postdata)) {
     // required Params are set
     if (isset($request->subject) && isset($request->email) && isset($request->message))  {
 
-        // Send Mail to Me
+        // ======== Send Mail to Me ========
         $visitor_email = $request->email;
         $my_email = "tobias@karzewski.de";
         $subject = $request->subject;
         $message = "Nachricht von der Webseite\n\n----\n\n" . $request->message;
-        mail($my_email, "$subject", $message, "From:" . $visitor_email);
+        $headers   = array();
+        $headers[] = "MIME-Version: 1.0";
+        $headers[] = "Content-type: text/plain; charset=utf-8";
+        $headers[] = "From: {$visitor_email}";
+        $headers[] = "Reply-To: {$visitor_email}";
+        $headers[] = "Subject: {$subject}";
+        $headers[] = "X-Mailer: PHP/".phpversion();
+        mail($my_email, "$subject", $message, implode("\r\n",$headers));
 
-        // Send Copy of the message to visitor
+
+        // ======== Send Copy of the message to visitor ========
         $subject_copy = "Ihre Anfrage an Tobias Karzewski";
         $message_copy = "Hallo und schönen guten Tag!\n\nVielen Dank für Ihre Anfrage über meine Webseite http://www.tobias-karzewski.de\n" .
             "Ich werde Ihre untenstehende Nachricht in Kürze beantworten.\n\nViele Grüße\n\n Tobias Karzewski - Selbstständiger Softwareentwickler\n\n\n" .
             "==== Kopie Ihrer Nachricht ====\n\n" . $request->message;
-        mail($visitor_email, "$subject_copy", $message_copy, "From:" . $my_email);
+        $headers   = array();
+        $headers[] = "MIME-Version: 1.0";
+        $headers[] = "Content-type: text/plain; charset=utf-8";
+        $headers[] = "From: {$my_email}";
+        $headers[] = "Reply-To: {$my_email}";
+        $headers[] = "Subject: {$subject_copy}";
+        $headers[] = "X-Mailer: PHP/".phpversion();
+        mail($visitor_email, "$subject_copy", $message_copy, implode("\r\n",$headers));
 
         //Email response
         echo "true";
