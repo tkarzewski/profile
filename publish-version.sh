68fd5c57a2ad52633a266a16fcad2709843921c2
Tobias Karzewski <tobias@karzewski.de>
2018-09-10 21:06:47 +0200

    Updated Readme

---
+++ publish-version.sh
@@ -0,0 +1,20 @@
+# TODO Doc
+
+# Check for Version-Param and set or get version
+# if param set version
+# else get version
+
+# build version
+# check for failure
+# replace base-href
+
+# zip dist folder
+# copy dist.zip to webspace 'profile_workdir'
+
+# empty 'profile'-dir
+# unzip dist.zip into 'profile'-dir
+
+
+# scp foobar.txt your_username@remotehost.edu:/some/remote/directory
+
+# ssh u72846928@life-is-a-book.de 'mkdir karzewski_workdir'
