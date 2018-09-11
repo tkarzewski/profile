06f60adbd6fb5efc3480828bad39144453fbff0e
Tobias Karzewski <tobias.karzewski@barmenia.de>
2018-09-11 13:28:50 +0200

    Initial deploy-script

--- publish-version.sh
+++ publish-version.sh
@@ -1,20 +1,55 @@
 # TODO Doc
 
-# Check for Version-Param and set or get version
-# if param set version
-# else get version
 
-# build version
-# check for failure
-# replace base-href
+# ===== get version from package.json or param =====
+profile_version=$1
+if [ -z $1 ]; then
+  profile_version=$(grep '"version":' ./package.json |cut -d: -f2|tr -d ','|tr -d ' '|tr -d '"')
+fi
+echo "Profile Version is $profile_version\n"
 
-# zip dist folder
-# copy dist.zip to webspace 'profile_workdir'
 
-# empty 'profile'-dir
+# ===== delete old dist folder =====
+echo "===== Delete old dist folder ====="
+rm -rf ./dist
+
+
+# ===== build version =====
+echo "===== Build $profile_version with --prod --base-href / ====="
+ng build --prod --base-href /
+
+
+# ===== check for failure =====
+returnValue=$?
+if [ $returnValue != 0 ];then
+   echo "===== Angular Build failed :(  ====="
+   exit
+fi
+
+
+# ===== replace base-href =====
+echo "===== Replace Tag base-href with / ====="
+sed -ri 's/(base href=")[^"]+"/\1\/"/' ./dist/index.html
+
+
+# ===== zip dist folder =====
+echo "===== Create ZIP-File with contents of dist-Folder ====="
+cd dist/
+zip -r ../dist-$profile_version.zip *
+returnValue=$?
+cd ..
+
+
+# ssh u72846928@life-is-a-book.de 'mkdir /karzewski_workdir'
+# copy dist.zip to webspace '/karzewski_workdir'
+# empty 'karzewski'-dir
+# scp foobar.txt u72846928@life-is-a-book.de:/karzewski_workdir
 # unzip dist.zip into 'profile'-dir
 
 
-# scp foobar.txt your_username@remotehost.edu:/some/remote/directory
+# ===== delete uipped dist file =====
+echo "===== Delete uipped dist file ====="
+rm -f ./dist-$profile_version.zip
 
-# ssh u72846928@life-is-a-book.de 'mkdir karzewski_workdir'
+
+return $returnValue
